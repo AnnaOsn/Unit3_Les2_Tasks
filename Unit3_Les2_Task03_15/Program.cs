96ceb417961ccf8b3e1b9dba188194da49ca9918
Anna_Osn <Anya_kas@live.ru>
2022-12-20 13:42:57 +0300

    15 Задача

---
+++ Unit3_Les2_Task03_15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+// 2. По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели: ");
+
+int a = int.Parse(Console.ReadLine()!);
+
+if (a <= 5)
+{Console.WriteLine("Будни");}
+else
+{Console.WriteLine("Выходной");}
+
+if (a == 1)
+{     Console.WriteLine("понедельник");}
+if (a == 2)
+{     Console.WriteLine("вторник");}
+if (a == 3)
+{     Console.WriteLine("среда");}
+if (a == 4)
+{     Console.WriteLine("четверг");}
+if (a == 5)
+{     Console.WriteLine("пятница");}
+if (a == 6)
+{     Console.WriteLine("суббота");}
+if (a == 7)
+{     Console.WriteLine("воскресенье");}
